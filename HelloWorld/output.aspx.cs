964e8565b03a4f7bec947cd08f513a904ec77fcf
意轩 杨 <yangyi_xuan@163.com>
2019-02-22 17:04:52 +0800

    2019.2.22

---
+++ HelloWorld/output.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWorld
+{
+    public partial class output : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<script language='javascript'>alert('欢迎学习数据思维下的算法与程序设计')</script>");
+        }
+    }
+}
